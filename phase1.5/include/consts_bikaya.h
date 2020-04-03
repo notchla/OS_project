e94d98360101a794ccf8c34bedc20dd965872a60
gerkone <gianlucagalletti@ymail.com>
2020-04-03 17:25:17 +0200

    cause_all_get

--- phase1.5/include/consts_bikaya.h
+++ phase1.5/include/consts_bikaya.h
@@ -11,13 +11,19 @@
 #define WAIT_IO_DEVICE            8
 
 //interrupt lines
-#define LOCALTIME_INT             2
-#define INTERVALTIME_INT          4
-#define DISK_INT                  8
-#define TAPE_INT                  16
-#define NETWORK_INT               32
-#define PRINTER_INT               64
-#define TERM_INT                  128
+#define LOCALTIME_INT             0x00000002
+#define INTERVALTIME_INT          0x00000004
+#define DISK_INT                  0x00000008
+#define TAPE_INT                  0x00000010
+#define NETWORK_INT               0x00000020
+#define PRINTER_INT               0x00000040
+#define TERM_INT                  0x00000080
 
 //filter interrupt cause bits
 #define CAUSE_MASK                0x000000FF
+
+//archidecture specific custom macros
+#ifdef TARGET_UARM
+/* returns the complete cause vector */
+#define CAUSE_ALL_GET(cause) ((cause & 0xFF000000) >> 24)
+#endif
